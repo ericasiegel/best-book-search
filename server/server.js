fbb4dbd400dd31b3d9cc0a6c35e0bb0bf61ac23a
Erica Siegel <siegel.erica@gmail.com>
2021-03-23 13:03:45 -0700

    modified the server.js and auth.js to include apollo and graphql authentication

--- server/server.js
+++ server/server.js
@@ -1,21 +1,44 @@
 const express = require('express');
-const path = require('path');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+// import the db connection
 const db = require('./config/connection');
-const routes = require('./routes');
+// import the authorization middleware
+const { authMiddleware } = require('./utils/auth');
+
+// const path = require('path');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // pass throught the authMiddleware to ensure that every request performs an authentication check
+  context: authMiddleware
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,20 +5,28 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  // expect a user object and add that user's username, email, and id to the token
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.query.token || req.body.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
     }
-
+    // if there is no token, return the requested object as is
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
-
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
@@ -28,12 +36,8 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return updated request object
+    return req;
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  
 };
