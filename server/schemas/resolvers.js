494d9c9b926a4927862002b95020a361dbbd4137
Erica Siegel <siegel.erica@gmail.com>
2021-03-23 13:50:55 -0700

    added login functionality and authentication to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,7 @@ const { User, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 // import signToken from auth.js
 const { signToken } = require('../utils/auth');
+const { sign } = require('jsonwebtoken');
 
 const resolvers = {
     Query: {
@@ -19,6 +20,36 @@ const resolvers = {
             }
             // if no 'context.user' property exists then throw new AuthenticationError
             throw new AuthenticationError('Not logged in!');
+        }
+    },
+    Mutation: {
+        // add a user
+        addUser: async (parent, args) => {
+            // user data is passed in as args
+            const user = await User.create(args);
+            // combine the user data with the token
+            const token = signToken(user);
+            // create the token for the new user and return token and user
+            return { token, user };
+        },
+        // user login
+        login: async (parent, { email, password }) => {
+            // find the user by provided email
+            const user = await User.findOne({ email })
+            // if there isn't a user by that email throw the new AuthenticationError
+            if (!user) {
+                throw new AuthenticationError('Incorrect Username!');
+            }
+            // check to see if the password is correct
+            const correctPw = await user.isCorrectPassword(password);
+            // if the password is incorrect throw the new AuthenticationError
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect Password!');
+            }
+
+            // create the token for the user login and return token and user
+            const token = signToken(user);
+            return { token, user };
         }
     }
 }
